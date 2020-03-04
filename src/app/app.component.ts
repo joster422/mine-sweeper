7a1749ecbcaee472cbbe1d953ba0a35c4b5b8e12
Joster422 <Joster422@gmail.com>
2020-03-03 23:14:25 -0500

    chore - gh-pages deploy

--- src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ms-root',
-  template: '<ms-game></ms-game>'
-})
-export class AppComponent { }
--- src/app/game/form/form.ts
+++
@@ -1,6 +0,0 @@
-export class Form {
-  rows = 10;
-  columns = 10;
-  mines = 15;
-  isBotEnabled = false;
-}
